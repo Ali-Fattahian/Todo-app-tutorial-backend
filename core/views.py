f9c2bf15c7d24aef986085c7758325261ba2b1e5
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-07-25 15:18:29 +0430

    add login route and view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
+@api_view(['POST'])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    if not (username and password): # If either of them is empty
+        return Response({'error': 'Please provide username and password'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    user = authenticate(username=username, password=password) # Returns user if authentication was successfull
+    if user: # Success
+        token = Token.objects.get_or_create(user=user) # Tuple like this: (token, created-or-not)
+        return Response(str(token[0]), status=status.HTTP_200_OK)
+    
+    return Response({'error': 'Username or password is wrong'}, status=status.HTTP_401_UNAUTHORIZED)
