5aa222f840bde067d5b17c3ce9146e7db8e9969f
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-07-25 14:36:05 +0430

    connect core app, make todo model, make then run all migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Todo(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE) # If the author object is deleted, Delete all todos related to it
+    title = models.CharField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(blank=False, default=False)
+
+    def __str__(self):
+        return f'{self.author.username}\'s Todo'
